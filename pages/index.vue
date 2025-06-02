4733f219008eaa6dfd75516db79f10c0d19fb75c
Okigwe Ebube Ireneaus <alfredebube7@gmail.com>
2025-06-02 21:26:40 +0100

    101

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,35 @@
 <template>
-  <main class="px-5">
+  <main>
     <MainHero />
-     <MainAboutMyself />
-      <MainMyServiceComp />
-      <MainSpecialSkill />
+    <MainAboutMyself />
+    <MainMyServiceComp />
+    <MainSpecialSkill />
 
     <div class="card" id="projects">
-      <div class="card-body" >
-        <MainTitle title="My Projects" />
+      <div class="card-body">
+         <!-- Start Section Content -->
+            <div class="section-content">
+              <span class="section-tag" data-aos="fade-up">projects</span>
+              <h2 class="section-title" data-aos="fade-right">
+                My Complete Projects
+              </h2>
+            </div>
+            <!-- End Section Content -->
         <br />
 
-        <div class="grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-x-1 gap-y-4">
+        <div
+          class="grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-x-1 gap-y-4"
+        >
           <MainMiniProjectCard
             :project="pr"
-            v-for="pr, index in projects"
+            v-for="(pr, index) in projects"
             :key="index"
-           
           />
-        </div>    </div>
+        </div>
+      </div>
     </div>
 
-    <div id="blog"  class="card">
+    <div id="blog" class="card">
       <div class="card-body">
         <MainTitle title="Must Read" />
         <br />
@@ -32,7 +41,7 @@
 
       <div class="card">
         <div
-          class="card-body grid lg:grid-cols-2 gap-y-5  place-content-center min-h-[500px]"
+          class="card-body grid lg:grid-cols-2 gap-y-5 place-content-center min-h-[500px]"
         >
           <div class="flex flex-col justify-center px-6">
             <MainTitle title="Contact Me" />
@@ -41,18 +50,28 @@
               expert web development tailored to your needs. Let’s bring your
               vision to life—contact me today!
             </div>
-            <div class="mb-7 flex flex-col md:flex-row gap-4" >
+            <div class="mb-7 flex flex-col md:flex-row gap-4">
               <div class="card rounded" data-aos="fade-right">
-                <div class="card-body rounded bg-slate-950" >
+                <div class="card-body rounded bg-slate-950">
                   <i class="fa fa-envelope text-secondary"></i>
-                  <a href="mailto:ebubeireneaus@gmail.com" target="_blank" rel="noopener noreferrer">ebubeireneaus@gmail.com</a>
+                  <a
+                    href="mailto:ebubeireneaus@gmail.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >ebubeireneaus@gmail.com</a
+                  >
                 </div>
               </div>
 
               <div class="card" data-aos="fade-up">
                 <div class="card-body rounded bg-slate-950">
                   <i class="fab fa-whatsapp text-secondary"></i>
-                  <a  href="https://wa.me/+2348061982520" target="_blank" rel="noopener noreferrer">+234 806 198 2520</a>
+                  <a
+                    href="https://wa.me/+2348061982520"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >+234 806 198 2520</a
+                  >
                 </div>
               </div>
             </div>
@@ -60,20 +79,20 @@
             <div class="card" data-aos="fade-up">
               <div class="card-body rounded bg-slate-950">
                 <h6 class="text-xl mb-3">Socials</h6>
-            <div class="flex gap-x-5 *:text-slate-300">
-              <a href="http://" target="_blank" rel="noopener noreferrer"
-                ><i class="fab fa-facebook fa-lg"></i
-              ></a>
-              <a href="http://" target="_blank" rel="noopener noreferrer"
-                ><i class="fab fa-twitter fa-lg"></i
-              ></a>
-              <a href="http://" target="_blank" rel="noopener noreferrer"
-                ><i class="fab fa-linkedin fa-lg"></i
-              ></a>
-              <a href="http://" target="_blank" rel="noopener noreferrer"
-                ><i class="fab fa-tiktok fa-lg"></i
-              ></a>
-            </div>
+                <div class="flex gap-x-5 *:text-slate-300">
+                  <a href="http://" target="_blank" rel="noopener noreferrer"
+                    ><i class="fab fa-facebook fa-lg"></i
+                  ></a>
+                  <a href="http://" target="_blank" rel="noopener noreferrer"
+                    ><i class="fab fa-twitter fa-lg"></i
+                  ></a>
+                  <a href="http://" target="_blank" rel="noopener noreferrer"
+                    ><i class="fab fa-linkedin fa-lg"></i
+                  ></a>
+                  <a href="http://" target="_blank" rel="noopener noreferrer"
+                    ><i class="fab fa-tiktok fa-lg"></i
+                  ></a>
+                </div>
               </div>
             </div>
           </div>
@@ -88,12 +107,13 @@
 </template>
 
 <script setup lang="ts">
-import { mySkills } from '~/lib/skills';
-import { Projects as projects}  from '~/lib/projects';
+import { mySkills } from "~/lib/skills";
+import { Projects as projects } from "~/lib/projects";
 
 const blogs = ref<any>([]);
-const { data: getBlogs } = await useFetch<any>("/api/main/blog/all?len=10&mini=true");
-
+const { data: getBlogs } = await useFetch<any>(
+  "/api/main/blog/all?len=10&mini=true"
+);
 
 if (getBlogs.value) {
   blogs.value = getBlogs.value;
